1dd485bd43289ad85517743307595230ad0d004f
Ren <Ren@192.168.11.5>
2021-08-27 16:09:25 +0900

    8/27 16:00時点

--- src/test/java/org/docksidestage/javatry/basic/st6/dbms/Sql.java
+++ src/test/java/org/docksidestage/javatry/basic/st6/dbms/Sql.java
@@ -4,6 +4,12 @@ package org.docksidestage.javatry.basic.st6.dbms;
  * @author nagano
  * */
 
-public interface Sql {
-    public String buildPagingQuery(int pageSize, int pageNumber);
+public abstract class Sql {
+    public int offset;
+
+    abstract public String buildPagingQuery(int pageSize, int pageNumber);
+
+    protected int getOffset() {
+        return pageSize * (pageNumber - 1); //???????????????????????
+    }
 }
